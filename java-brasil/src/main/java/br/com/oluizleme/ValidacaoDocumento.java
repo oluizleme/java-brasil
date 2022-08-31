be144e167138f01f3bc498538af2203494286ced
o.luizleme <o.luizleme@gmail.com>
2022-08-31 20:13:49 -0300

    refactor(main): Refatorada a classe de validacao de documentos

--- java-brasil/src/main/java/br/com/oluizleme/ValidacaoDocumento.java
+++ java-brasil/src/main/java/br/com/oluizleme/ValidacaoDocumento.java
@@ -1,18 +1,36 @@
 package br.com.oluizleme;
 
-import br.com.caelum.stella.validation.CPFValidator;
-import br.com.caelum.stella.validation.InvalidStateException;
+import br.com.caelum.stella.validation.*;
 
 public class ValidacaoDocumento {
 
     public static void main(String[] args) {
         String cpf = "75826591552";
-        CPFValidator validator = new CPFValidator();
         try{
-            validator.assertValid(cpf);
+            validarDocumento(new CPFValidator(), cpf);
             System.out.println("CPF VÁLIDO");
         } catch (InvalidStateException e) {
             System.out.println("CPF INVÁLIDO : " + e);
         }
+
+        String cnpj = "59863685000142";
+        try{
+            validarDocumento(new CNPJValidator(), cnpj);
+            System.out.println("CNPJ VÁLIDO");
+        } catch (InvalidStateException e) {
+            System.out.println("CNPJ INVÁLIDO : " + e);
+        }
+
+        String tituloDeEleitor = "235881840183";
+        try {
+           validarDocumento(new TituloEleitoralValidator(), tituloDeEleitor);
+            System.out.println("TÍTULO VÁLIDO");
+        } catch (InvalidStateException e) {
+            System.out.println("TÍTULO INVÁLIDO :" + e);
+        }
+    }
+
+    private static void validarDocumento(Validator<String> validador, String documento) {
+        validador.assertValid(documento);
     }
 }
